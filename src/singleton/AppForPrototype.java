9c2823e2e708ac9d458efa01dc16fa58feabb995
Aeon Tanvir <aeontanvir@gmail.com>
2023-09-19 01:00:09 -0500

    Prototype design patter implemented

---
+++ src/singleton/AppForPrototype.java
@@ -0,0 +1,20 @@
+package singleton;
+
+public class AppForPrototype {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        BookShop bookShop = new BookShop();
+        bookShop.setShopName("Tanvir Books Shop");
+        bookShop.loadData();
+        System.out.println(bookShop);
+        System.out.println("----------\n");
+
+
+        BookShop otherShop = bookShop.clone();
+        bookShop.getBooks().remove(9);
+        otherShop.setShopName("Other Books Shop");
+        //bookShop.loadData();
+        System.out.println(otherShop);
+        System.out.println("----------\n");
+        System.out.println(bookShop);
+    }
+}
